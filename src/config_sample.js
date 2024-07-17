bc9128f496bafc1d30c696371a790c77ebbdf54c
haomrvuii2001 <nguyentukhoa11@gmail.com>
2024-07-17 09:20:58 +0700

    first commit

---
+++ src/config_sample.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://qda13rgqrc.execute-api.us-east-1.amazonaws.com/default"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_viFnwuA08",
+        APP_CLIENT_ID: "428m8eg57tbc95tqpu2f0uju19",
+        IDENTITY_POOL_ID: "us-east-1:353439cb-0015-4a05-b385-b1a072fb3814"
+    }
+}
